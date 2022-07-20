381d58fa0b80acdc3e2504c47f8bdee90764b11b
Marcos Vinícius <mviniciusmarquesvk@hotmail.com>
2022-07-20 11:25:05 -0300

    section 4 adicionado e interação com js

---
+++ assets/style/script.js
@@ -0,0 +1,181 @@
+/* Fiz as funções selecionando individualmente cada elemento da Section 4, pois não tem como usar o .addEventListener 
+com o .querySelectorAll para selecionar todos os elementos de uma vez só. */
+
+let div1 = document.querySelector('.picture1')
+let pic1 = document.querySelector('.content-picture1') 
+
+div1.addEventListener('mouseover', over1)
+div1.addEventListener('mouseout', out1) 
+
+function over1() {
+    pic1.style.display = 'flex'
+}
+function out1() {
+    pic1.style.display = 'none'
+}
+
+
+
+
+let div2 = document.querySelector('.picture2') 
+let pic2 = document.querySelector('.content-picture2')
+
+div2.addEventListener('mouseover', over2)
+div2.addEventListener('mouseout', out2) 
+
+function over2() {
+    pic2.style.display = 'flex'
+}
+function out2() {
+    pic2.style.display = 'none'
+}
+
+
+
+let div3 = document.querySelector('.picture3') 
+let pic3 = document.querySelector('.content-picture3')
+
+div3.addEventListener('mouseover', over3)
+div3.addEventListener('mouseout', out3) 
+
+function over3() {
+    pic3.style.display = 'flex'
+}
+function out3() {
+    pic3.style.display = 'none'
+}
+
+
+
+let div4 = document.querySelector('.picture4') 
+let pic4 = document.querySelector('.content-picture4')
+
+div4.addEventListener('mouseover', over4)
+div4.addEventListener('mouseout', out4) 
+
+function over4() {
+    pic4.style.display = 'flex'
+}
+function out4() {
+    pic4.style.display = 'none'
+}
+
+
+
+let div5 = document.querySelector('.picture5') 
+let pic5 = document.querySelector('.content-picture5')
+
+div5.addEventListener('mouseover', over5)
+div5.addEventListener('mouseout', out5) 
+
+function over5() {
+    pic5.style.display = 'flex'
+}
+function out5() {
+    pic5.style.display = 'none'
+}
+
+
+
+let div6 = document.querySelector('.picture6') 
+let pic6 = document.querySelector('.content-picture6')
+
+div6.addEventListener('mouseover', over6)
+div6.addEventListener('mouseout', out6) 
+
+function over6() {
+    pic6.style.display = 'flex'
+}
+function out6() {
+    pic6.style.display = 'none'
+}
+
+
+
+let div7 = document.querySelector('.picture7') 
+let pic7 = document.querySelector('.content-picture7')
+
+div7.addEventListener('mouseover', over7)
+div7.addEventListener('mouseout', out7) 
+
+function over7() {
+    pic7.style.display = 'flex'
+}
+function out7() {
+    pic7.style.display = 'none'
+}
+
+
+
+let div8 = document.querySelector('.picture8') 
+let pic8 = document.querySelector('.content-picture8')
+
+div8.addEventListener('mouseover', over8)
+div8.addEventListener('mouseout', out8) 
+
+function over8() {
+    pic8.style.display = 'flex'
+}
+function out8() {
+    pic8.style.display = 'none'
+}
+
+
+
+let div9 = document.querySelector('.picture9') 
+let pic9 = document.querySelector('.content-picture9')
+
+div9.addEventListener('mouseover', over9)
+div9.addEventListener('mouseout', out9) 
+
+function over9() {
+    pic9.style.display = 'flex'
+}
+function out9() {
+    pic9.style.display = 'none'
+}
+
+
+
+let div10 = document.querySelector('.picture10') 
+let pic10 = document.querySelector('.content-picture10')
+
+div10.addEventListener('mouseover', over10)
+div10.addEventListener('mouseout', out10) 
+
+function over10() {
+    pic10.style.display = 'flex'
+}
+function out10() {
+    pic10.style.display = 'none'
+}
+
+
+
+let div11 = document.querySelector('.picture11') 
+let pic11 = document.querySelector('.content-picture11')
+
+div11.addEventListener('mouseover', over11)
+div11.addEventListener('mouseout', out11) 
+
+function over11() {
+    pic11.style.display = 'flex'
+}
+function out11() {
+    pic11.style.display = 'none'
+}
+
+
+
+let div12 = document.querySelector('.picture12') 
+let pic12 = document.querySelector('.content-picture12')
+
+div12.addEventListener('mouseover', over12)
+div12.addEventListener('mouseout', out12) 
+
+function over12() {
+    pic12.style.display = 'flex'
+}
+function out12() {
+    pic12.style.display = 'none'
+}
